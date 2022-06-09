bc854c2f5dc9d2bcee2a5ea186d07637186905cd
BNew2022 <brentnewman223@gmail.com>
2022-06-09 12:04:29 -0400

    Deliverable 2 code complete

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -18,3 +18,7 @@ lm(mpg ~ AWD,MechaCar)
 summary(lm(mpg ~ AWD,MechaCar))
 
 #Deliverable 2
+Suspension_Coil <- read.csv(file='Suspension_Coil.csv')
+total_summary <- Suspension_Coil %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+                                               
