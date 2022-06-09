22c81969a4fd51090652a8c18d34abf6cac8fd35
BNew2022 <brentnewman223@gmail.com>
2022-06-09 11:45:05 -0400

    Deliverable 1 code complete

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,2 +1,20 @@
-library(tidyverse)
+#Deliverable 1
+library(tidyverse)      
 MechaCar <- read.csv(file='MechaCar_mpg.csv')
+
+lm(mpg~vehicle_length,MechaCar)
+summary(lm(mpg~vehicle_length,MechaCar))
+
+lm(mpg~vehicle_weight,MechaCar)
+summary(lm(mpg~vehicle_weight,MechaCar))
+
+lm(mpg ~ spoiler_angle,MechaCar)
+summary(lm(mpg ~ spoiler_angle,MechaCar))
+
+lm(mpg ~ ground_clearance,MechaCar)
+summary(lm(mpg ~ ground_clearance,MechaCar))
+
+lm(mpg ~ AWD,MechaCar)
+summary(lm(mpg ~ AWD,MechaCar))
+
+#Deliverable 2
