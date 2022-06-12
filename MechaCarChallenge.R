825251fe9b5bb478dd381cdb521b669b4588cdfe
BNew2022 <brentnewman223@gmail.com>
2022-06-12 12:47:50 -0400

    Challenge Code complete

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -2,20 +2,19 @@
 library(tidyverse)      
 MechaCar <- read.csv(file='MechaCar_mpg.csv')
 
-lm(mpg~vehicle_length,MechaCar)
-summary(lm(mpg~vehicle_length,MechaCar))
+#lm(mpg~vehicle_length,MechaCar)
+#summary(lm(mpg~vehicle_length,MechaCar))
+#lm(mpg~vehicle_weight,MechaCar)
+#summary(lm(mpg~vehicle_weight,MechaCar))
+#lm(mpg ~ spoiler_angle,MechaCar)
+#summary(lm(mpg ~ spoiler_angle,MechaCar))
+#lm(mpg ~ ground_clearance,MechaCar)
+#summary(lm(mpg ~ ground_clearance,MechaCar))
+#lm(mpg ~ AWD,MechaCar)
+#summary(lm(mpg ~ AWD,MechaCar))
 
-lm(mpg~vehicle_weight,MechaCar)
-summary(lm(mpg~vehicle_weight,MechaCar))
-
-lm(mpg ~ spoiler_angle,MechaCar)
-summary(lm(mpg ~ spoiler_angle,MechaCar))
-
-lm(mpg ~ ground_clearance,MechaCar)
-summary(lm(mpg ~ ground_clearance,MechaCar))
-
-lm(mpg ~ AWD,MechaCar)
-summary(lm(mpg ~ AWD,MechaCar))
+comparison <- lm(mpg~vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,MechaCar)
+summary(comparison)
 
 #Deliverable 2
 Suspension_Coil <- read.csv(file='Suspension_Coil.csv')
@@ -23,4 +22,7 @@ total_summary <- Suspension_Coil %>% summarize(Mean=mean(PSI),Median=median(PSI)
 lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
 
 #Deliverable 3
-t.test(log10(Suspension_Coil$PSI),mu=1500)
+t.test((Suspension_Coil$PSI),mu=1500)
+t.test(subset(Suspension_Coil,Manufacturing_Lot=="Lot1")$PSI,mu=1500)
+t.test(subset(Suspension_Coil,Manufacturing_Lot=="Lot2")$PSI,mu=1500)
+t.test(subset(Suspension_Coil,Manufacturing_Lot=="Lot3")$PSI,mu=1500)
